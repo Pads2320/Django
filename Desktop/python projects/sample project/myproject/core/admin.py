02547eed040ba2e29415bbed7959c90c936d61d6
ITSD Dev Intern 3 <itsddev.intern3@mgenesis.com>
2025-02-27 14:08:37 +0800

    Removed unnecessary folder from repository

--- Desktop/python projects/sample project/myproject/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Post, Comment
-
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(Post)
-admin.site.register(Comment)
